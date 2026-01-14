47adb4da1b36dbb8135669566d942467b4eca18f
ziyanx02 <ziyanx02@gmail.com>
2026-01-14 17:15:15 -0500

    minor change

--- examples/run_ppo_motion.py
+++ examples/run_ppo_motion.py
@@ -307,7 +307,7 @@ def evaluate_policy(
                     foot_link_pos += env.ref_base_pos
                     env.scene.scene.draw_debug_arrow(
                         foot_link_pos,
-                        env.ref_foot_contact_weighted[0, i]
+                        env.ref_foot_contact[0, i]
                         * torch.tensor([0.0, 0.0, 0.5], device=env.device),
                         radius=0.01,
                         color=(0.0, 0.0, 1.0),
